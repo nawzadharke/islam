51d7d7ac9eff19d678fa8e68fee96238f57f3815
nawzadharke <118609890+nawzadharke@users.noreply.github.com>
2022-11-19 15:20:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function zero() {
+
+    document.getElementById('zero').style.display = "none";
+    document.getElementById('one').style.display = "block";
+
+}
+function one() {
+
+    document.getElementById('one').style.display = "none";
+    document.getElementById('tow').style.display = "block";
+
+}
+function tow() {
+
+    document.getElementById('tow').style.display = "none";
+    document.getElementById('thre').style.display = "block";
+}
+function thre() {
+
+    document.getElementById('thre').style.display = "none";
+    document.getElementById('four').style.display = "block";
+}
+function four() {
+
+    document.getElementById('four').style.display = "none";
+    document.getElementById('zero').style.display = "block";
+}
